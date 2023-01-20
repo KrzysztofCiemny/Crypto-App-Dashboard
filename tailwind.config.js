eb60361479b03bffea469c6dbf0fbf6a6cdfb08f
KrzysztofCiemny <ciemnykrzysztof97@gmail.com>
2023-01-20 18:32:26 +0100

    adding theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gray-bg': '#F7F7F9',
+        'gray-text': '#9896A1',
+        black: '#0A041C',
+        green: '#2DC78F',
+        red: '#EA4D4D',
+        purple: '#7445FB',
+      },
+    },
   },
   plugins: [],
-}
+};
